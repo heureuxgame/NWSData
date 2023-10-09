c0ac70326f9a1a3e2e2f686efccbeec3092f2550
heureuxgame <yleiden@hotmail.com>
2023-10-09 14:56:30 -0400

    a1

--- app/src/main/java/com/yaleiden/nwsdata/NwsApiService.kt
+++ app/src/main/java/com/yaleiden/nwsdata/NwsApiService.kt
@@ -1,9 +1,11 @@
 package com.yaleiden.nwsdata
 
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+
 
 /*
 How do I determine the gridpoint for my location?
@@ -17,22 +19,50 @@ https://api.weather.gov/gridpoints/LSX/94,73/forecast/ as your BASE_URL
 from properties:  forecast
 this is your gridpoint
  */
-    private val BASE_URL = "https://api.weather.gov/gridpoints/CAE/15,40/forecast/"
-    //private val LAT_LON =        "38.6270,-90.1994"
 
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .build()
+// Location Evans, GA
+//private val BASE_URL = "https://api.weather.gov/gridpoints/CAE/15,40/forecast/"
 
-    interface NwsApiService {
-        @GET("hourly")
-        suspend fun getHourlyForecast(): Response<String>
-        //suspend fun getHourlyForecast(): Response<ForecastHourlyData>
+// Location for Clay Hill, GA
+const val LOCATION = "Clay Hill, GA"
+private val BASE_URL = "https://api.weather.gov/gridpoints/CAE/14,41/forecast/"
+
+// Location for Hawks Rest USFS Patrol Cabin, Bridger-Teton NF, WY
+//const val LOCATION = "Hawks Rest BTNF, WY"
+//private val BASE_URL = "https://api.weather.gov/gridpoints/RIW/67,165/forecast/"
+
+//private val LAT_LON =        "38.6270,-90.1994"
+//private val LAT_LON =        "33.5358,-82.1676"  //Local
+//private val BASE_URL = "https://api.weather.gov/gridpoints/CAE/25,36/forecast"
+
+//private val UA = System.getProperty("http.agent")  // Get android user agent.
+
+
+
+val userAgentInterceptor = UserAgentInterceptor()
+
+// OkHttpClient. Be conscious with the order
+var okHttpClient = OkHttpClient()
+    .newBuilder()
+    .addInterceptor(userAgentInterceptor) // interceptor for adding User-Agent
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .client(okHttpClient)
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NwsApiService {
+    @GET("hourly")
+    suspend fun getHourlyForecast(): Response<String>
+    //suspend fun getHourlyForecast(): Response<ForecastHourlyData>
+
+}
+
+object NwsApi {
+    val retrofitService: NwsApiService by lazy {
+        retrofit.create(NwsApiService::class.java)
     }
-
-    object NwsApi {
-        val retrofitService : NwsApiService by lazy {
-            retrofit.create(NwsApiService::class.java)
-        }
-    }
+    val location = LOCATION
+}
